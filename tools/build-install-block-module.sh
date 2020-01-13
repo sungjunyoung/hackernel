b769da22bb683792ba5f5eec934f5bee134bf281
Junyoung, Sung <junyoung.sung@navercorp.com>
2020-01-13 19:20:56 +0900

    Add modprobe message after build

--- tools/build-install-block-module.sh
+++ tools/build-install-block-module.sh
@@ -4,3 +4,7 @@ set +x
 cd /root/linux-4.19.52
 make -C . M=drivers/block -j $(nproc)
 cp drivers/block/rbd.ko /lib/modules/4.19.52/kernel/drivers/block/
+depmod
+
+echo "modprobe -r rbd"
+echo "modprobe rbd"
